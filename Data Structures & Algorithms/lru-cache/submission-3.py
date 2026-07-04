d8750d383613fcc051958de916cf74a677e2c94f
Aravind Srinivasan <stevethejobless@gmail.com>
2026-07-04 19:02:57 +0530

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.py
@@ -0,0 +1,32 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = []
+
+    def get(self, key: int) -> int:
+        index = self._get_index(key)
+        if index != None:
+            val = self.cache.pop(index)[key]
+            self.cache.append({key:val})
+            return val
+        return -1
+        
+
+    def _get_index(self,key):
+        for i,j in enumerate(self.cache):
+            if key in j:
+                return i
+
+    def put(self, key: int, value: int) -> None:
+        index = self._get_index(key)
+        if index != None:
+            self.cache.pop(index)
+        else:
+            if len(self.cache) >= self.cap:
+                self.cache.pop(0)
+        self.cache.append({key:value})
+
+
+        
+        
