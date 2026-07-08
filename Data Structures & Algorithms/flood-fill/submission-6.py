185646cae28c04e9a5fc411acd1c229c305aa9f3
Aravind Srinivasan <stevethejobless@gmail.com>
2026-07-08 18:13:06 +0530

    Add: flood-fill - submission-6

---
+++ Data Structures & Algorithms/flood-fill/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int, ref_color=None) -> List[List[int]]:
+        ROWS,COLS = len(image), len(image[0])
+        print(sr,sc,ref_color)
+        if ref_color == None:
+            ref_color = image[sr][sc]
+        if (min(sr,sc) < 0 or sr == ROWS or sc == COLS or 
+            color == image[sr][sc] or image[sr][sc] != ref_color):
+            return image
+        if image[sr][sc] == ref_color:
+            print(image[sr][sc],ref_color)
+            image[sr][sc] = color
+            print(image)
+        self.floodFill(image,sr+1,sc,color,ref_color)
+        self.floodFill(image,sr,sc+1,color,ref_color)
+        self.floodFill(image,sr,sc-1,color,ref_color)
+        self.floodFill(image,sr-1,sc,color,ref_color)
+        return image
