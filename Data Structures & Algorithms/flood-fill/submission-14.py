1ae5b76d92bfd03e8c5b9fc510a06c31848ce3b3
Aravind Srinivasan <stevethejobless@gmail.com>
2026-07-23 20:03:12 +0530

    Add: flood-fill - submission-14

---
+++ Data Structures & Algorithms/flood-fill/submission-14.py
@@ -0,0 +1,18 @@
+from collections import deque
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        ROWS,COLS = len(image), len(image[sr])
+        ori = image[sr][sc]
+        if ori == color:
+            return image
+        moves = [(1,0),(-1,0),(0,1),(0,-1)]
+        q = deque([(sr,sc)])
+        while q:
+            for i in range(0,len(q)):
+                r,c = q.popleft()
+                if 0<=r<ROWS and 0<=c<COLS and image[r][c] == ori:
+                    image[r][c] = color
+                    for move in moves:
+                        nr,nc = move
+                        q.append((r+nr,c+nc))
+        return image
