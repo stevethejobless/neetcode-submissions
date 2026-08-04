a2d2bd569829d99167f1ca56ddeecf1957de52e7
Aravind Srinivasan <stevethejobless@gmail.com>
2026-08-04 11:06:52 +0530

    Add: rotting-fruit - submission-9

---
+++ Data Structures & Algorithms/rotting-fruit/submission-9.py
@@ -0,0 +1,31 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        moves = [0,1,0,-1,0]
+        ticks = 0
+        fresh = 0
+        rottable = True
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 1:
+                    fresh+=1
+        while fresh>0:
+            rottable = False
+            for r in range(ROWS):
+                for c in range(COLS):
+                    if grid[r][c] == 2:
+                        for i in range(4):
+                            nr,nc = r+moves[i], c+moves[i+1]
+                            if nr in range(ROWS) and nc in range(COLS) and grid[nr][nc] == 1:
+                                grid[nr][nc] = 3
+                                rottable = True
+                                fresh-=1
+            ticks+=1
+            if not rottable:
+                break
+            for r in range(ROWS):
+                for c in range(COLS):
+                    if grid[r][c] == 3:
+                        grid[r][c] = 2
+
+        return ticks if fresh==0 else -1
